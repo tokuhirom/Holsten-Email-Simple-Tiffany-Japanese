2a3515914030852920d9941cc638c6c15e7e2408
tokuhirom <tokuhirom@gmail.com>
2010-10-08 15:08:39 +0900

    initial import

--- lib/Holsten/Email/Simple/Tiffany/Japanese.pm
+++ lib/Holsten/Email/Simple/Tiffany/Japanese.pm
@@ -3,8 +3,42 @@ use strict;
 use warnings;
 use 5.00800;
 our $VERSION = '0.01';
+use Tiffany;
+use Encode ();
+use Email::Simple;
+use Email::Simple::Creator;
 
+sub new {
+    my ( $class, $name, $args ) = @_;
+    my $tiffany = Tiffany->load( $name, $args );
+    return bless { tiffany => $tiffany }, $class;
+}
 
+sub render {
+    my ( $self, $tmpl, $args ) = @_;
+
+    my @lines = split /\n/, $self->{tiffany}->render( $tmpl, {%$args} );
+    my @headers;
+    while ( @lines > 0 && $lines[0] =~ /^([A-Z][A-Za-z_-]+)\s*:\s*(.+?)$/ ) {
+        my ( $key, $val ) = ( $1, $2 );
+        push @headers, $key, Encode::encode( 'MIME-Header-ISO_2022_JP', $val );
+        shift @lines;
+    }
+    if ( @lines > 0 && $lines[0] =~ /^\s*$/ ) {
+        shift @lines;
+    }
+    my $body = Encode::encode( 'iso-2022-jp', join( "\n", @lines ) );
+
+    return Email::Simple->create(
+        header     => \@headers,
+        body       => $body,
+        attributes => {
+            content_type => 'text/plain',
+            charset      => 'ISO-2022-JP',
+            encoding     => '7bit',
+        },
+    );
+}
 
 1;
 __END__
@@ -13,15 +47,36 @@ __END__
 
 =head1 NAME
 
-Holsten::Email::Simple::Tiffany::Japanese -
+Holsten::Email::Simple::Tiffany::Japanese - 日本語でメールを送信するときに楽するライブラリ
 
 =head1 SYNOPSIS
 
-  use Holsten::Email::Simple::Tiffany::Japanese;
+    use Holsten::Email::Simple::Tiffany::Japanese;
+    use Email::Sender::Simple qw/sendmail/;
+
+    my $estj = Holsten::Email::Simple::Tiffany::Japanese->new(
+        'Text::Xslate' => {
+            syntax => 'TTerse',
+            path   => ['./email_tmpl/'],
+        },
+    );
+    my $email = $estj->render('foo.eml', {token => $token});
+    sendmail($email);
 
 =head1 DESCRIPTION
 
-Holsten::Email::Simple::Tiffany::Japanese is
+日本語でメールを送信できます。
+
+テンプレートファイルには
+
+    Subject: [% name %]様へお特な情報のご案内
+
+    おとくですよ！
+    http://example.com[% path_info %]
+
+のようにかくことができます。
+
+最初のヘッダ行はなくてもかまいません。
 
 =head1 AUTHOR
 
---
+++ t/01_simple.t
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+use utf8;
+use Test::More;
+use Test::Requires 'Text::Xslate', 'Email::MIME';
+use Encode;
+use Holsten::Email::Simple::Tiffany::Japanese;
+
+{
+    my $e = Holsten::Email::Simple::Tiffany::Japanese->new(
+        'Text::Xslate' => {
+            path => './t/tmpl/',
+            syntax => 'TTerse',
+        },
+    );
+    my $mail = Email::MIME->new($e->render('foo.eml', {name => 'たろう', token => 'XZY'})->as_string);
+    is $mail->header('Subject'), 'たろう様こんにちは';
+    my $body = decode('iso-2022-jp', $mail->body);
+    is $body, "よろしくね\015\012http://example.com/token=XZY\015\012";
+}
+
+{
+    my $e = Holsten::Email::Simple::Tiffany::Japanese->new(
+        'Text::Xslate' => {
+            path => './t/tmpl/',
+            syntax => 'TTerse',
+        },
+    );
+    my $mail = Email::MIME->new($e->render('bar.eml', {name => 'たろう', token => 'XZY'})->as_string);
+    my $body = decode('iso-2022-jp', $mail->body);
+    is $body, "その2\015\012です。\015\012";
+}
+
+done_testing;
